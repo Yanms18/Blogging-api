de8173791f3c1d7babf5a8ab3f2e22cc03cce9a9
Yanms18 <158842637+Yanms18@users.noreply.github.com>
2024-12-16 09:16:30 +0100

    Create Blog.js

---
+++ models/Blog.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const blogSchema = new mongoose.Schema({
+  title: { 
+    type: String, 
+    required: true,
+    unique: true,
+    trim: true
+  },
+  description: { 
+    type: String 
+  },
+  author: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User', 
+    required: true 
+  },
+  state: { 
+    type: String, 
+    enum: ['draft', 'published'], 
+    default: 'draft' 
+  },
+  read_count: { 
+    type: Number, 
+    default: 0 
+  },
+  reading_time: { 
+    type: Number 
+  },
+  tags: [{ 
+    type: String 
+  }],
+  body: { 
+    type: String, 
+    required: true 
+  }
+}, { timestamps: true });
+
+export default mongoose.model('Blog', blogSchema);
