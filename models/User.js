87aebe8be7641a4d67456a5f0811fe5881ec96aa
Yanms18 <158842637+Yanms18@users.noreply.github.com>
2024-12-16 09:10:48 +0100

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  first_name: { 
+    type: String, 
+    required: true 
+  },
+  last_name: { 
+    type: String, 
+    required: true 
+  },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: { 
+    type: String, 
+    required: true 
+  },
+  blogs: [{ 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Blog' 
+  }]
+}, { timestamps: true });
+
+export default mongoose.model('User', userSchema);
