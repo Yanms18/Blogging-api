1140e5aaf2dd51b7a70fa347f9b365c406e5f46d
yanms18 <jejeoluwayanmife@gmail.com>
2024-12-19 18:35:37 +0100

    Refactor authentication module: clean up imports, remove unused code, and ensure JWT is properly integrated

--- Auth/auth.js
+++ Auth/auth.js
@@ -4,25 +4,11 @@ import { Strategy as JWTStrategy, ExtractJwt } from 'passport-jwt';
 import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
 import dotenv from 'dotenv';
-import express from 'express';
-import connectDB from './database/db.js';
-import userRoutes from './routes/userRoutes.js';
-import blogRoutes from './routes/blogRoutes.js';
-import authRoutes from './routes/authentication.js'; // Import authentication routes
+import jwt from 'jsonwebtoken'; // Ensure jwt is imported
+import connectDB from '../database/db.js'; // Corrected import path
 
 dotenv.config();
 connectDB();
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
-app.use('/api/users', userRoutes);
-app.use('/api/blogs', blogRoutes);
-app.use('/api/auth', authRoutes); // Register authentication routes
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
 
 // JWT Strategy
 const jwtOptions = {
@@ -56,31 +42,6 @@ passport.use('signup', new LocalStrategy({
     // Hash password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
-
-    
-// UserSchema.pre(
-//     'save',
-//     async function (next) {
-//         const user = this;
-//         const hash = await bcrypt.hash(this.password, 10);
-
-//         this.password = hash;
-//         next();
-//     }
-// );
-
-// // You will also need to make sure that the user trying to log in has the correct credentials. Add the following new method:
-// UserSchema.methods.isValidPassword = async function(password) {
-//     const user = this;
-//     const compare = await bcrypt.compare(password, user.password);
-  
-//     return compare;
-//   }
-
-
-// const UserModel = mongoose.model('users', UserSchema);
-
-
 
     // Create new user
     const newUser = new User({
